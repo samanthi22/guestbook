dc00228f7d9c07f1a96c6583f7888462c1cf84b6
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-22 14:50:37 +0000

    Added unless @name blank, create

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,9 @@
 class EntriesController < ApplicationController
     def sign_in
         @name = params[:visitor_name]
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
         @entry = Entry.create({:name => @name})
+        end
     end
 end
